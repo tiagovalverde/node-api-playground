6c0ad99fec94cb22394c73db467503be1e815da4
tiago.c.v17@gmail.com <tiago.c.v17@gmail.com>
2018-05-19 22:10:15 -0400

    Fix enviroment config

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ console.log(`*** Env: ${env} ***`);
 if(env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if ('test') {
+}else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
